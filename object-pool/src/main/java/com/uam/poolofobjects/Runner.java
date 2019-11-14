cf6442d8794b6be7be72268569077b36b857ff61
Adrian Antkowiak <s452653@labs.wmi.amu.edu.pl>
2019-11-14 09:37:57 +0100

    printing references in object-pool

--- object-pool/src/main/java/com/uam/poolofobjects/Runner.java
+++ object-pool/src/main/java/com/uam/poolofobjects/Runner.java
@@ -7,6 +7,7 @@ public class Runner {
         for (int i = 0; i < 10; i++) {
             System.out.println("getting a new tool");
             Tool tool = pool.getObject();
+            System.out.println("received " + tool);
             System.out.println("returning it");
             pool.returnObject(tool);
         }
